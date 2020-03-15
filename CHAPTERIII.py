7867dd7ab7f856abea2b87e85d7c8de9a823b504
mkaspryk <mkaspryk09@outlook.com>
2020-03-15 18:27:22 +0100

    task-3

--- CHAPTERIII.py
+++ CHAPTERIII.py
@@ -1,7 +1,15 @@
 # title: CHAPTER III
 # version: 1.01
-# date: 13/03/2020
+# date: 15/03/2020
 # author: Marcin Grzegorz Kaspryk
 
+# password is 6 digit number
+_begin = 372**2
+_end = 809**2
+print(_begin)
+# starts 138384
+print(_end)
+# ends 654481
+print("---------")
 
-
+print("Result: ",646)
